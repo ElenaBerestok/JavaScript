00823111b3c03aaba7bec137344f90f5d08cbe9b
ElenaBerestok <98764900+ElenaBerestok@users.noreply.github.com>
2022-11-12 19:13:22 +0200

    Add files via upload

---
+++ HW2/script/script.js
@@ -0,0 +1,65 @@
+// ДЗ пункт 1
+
+let userAge;
+
+userAge = 23;
+
+console.log(userAge);
+
+let userName;
+
+userName='Cristina';
+
+console.log(userName);
+
+let userSurname;
+
+userSurname = 'Shevchenko';
+
+console.log(userSurname);
+
+// ДЗ пункт 2
+
+// переменной, которая хранит нового пользователя
+
+let newUser;
+
+// переменной, которая хранит список покупок
+
+let shoppingList;
+
+// переменной, которая хранит информацию о том, является ли пользователь в сети (online)
+
+let userNetworkStatus;
+
+// переменной, которая хранит зарплату работника
+
+let employeeSalary;
+
+// переменной, которая хранит координаты курсора (мыши)
+
+let coordinatesCursor;
+
+// ДЗ пункт 3
+
+let greetings = 'Hello World';
+
+console.log(greetings);
+
+const text = 'const userAge = '
+
+console.log(`typeof text`, typeof text);
+
+const age = 12;
+
+console.log(`typeof age`, typeof age);
+
+const signature = text + age;
+
+console.log(signature);
+
+const textChange = 'userAge = '
+
+const signatureChange = textChange + age;
+
+console.log(signatureChange);
